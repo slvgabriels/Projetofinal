74ffe15608177f3a26d0ba694c0057c7b20f7d0d
tallisonferreira18-sketch <tallisonferreira18@gmail.com>
2025-11-18 13:56:31 -0300

    Update personagem-form.php

--- Projeto1-Tallison-F.-Miranda/Projeto1-Tallison-F.-Miranda-main/Projeto1 - Tallison F. Miranda/admin/personagem-form.php
+++ Projeto1-Tallison-F.-Miranda/Projeto1-Tallison-F.-Miranda-main/Projeto1 - Tallison F. Miranda/admin/personagem-form.php
@@ -10,6 +10,19 @@
     <input type="text" name="classe"><br>
     <label>Sub-classe:</label>
     <input type="text" name="subclasse"><br>
+    <label>Força:</label>
+    <input type="text" name="forca"><br>
+    <label>Destreza:</label>
+    <input type="text" name="destreza"><br>
+    <label>Constituição:</label>
+    <input type="text" name="constituicao"><br>
+    <label>Inteligência:</label>
+    <input type="text" name="inteligencia"><br>
+    <label>Sabedoria:</label>
+    <input type="text" name="sabedoria"><br>
+    <label>Carisma:</label>
+    <input type="text" name="carisma"><br>
 
     <input type="submit" value="Cadastrar personagem">
+
 </form>
